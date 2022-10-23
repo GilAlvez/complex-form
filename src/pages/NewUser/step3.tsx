d60a41de5830cfaf5359ec774c4d99677f6d4fdc
Gil Alves <gildson_af@outlook.com>
2022-10-23 14:54:54 -0300

    (feat create user) radio input handling error + applying next step handler into step 3

--- src/pages/NewUser/step3.tsx
+++ src/pages/NewUser/step3.tsx
@@ -1,17 +1,37 @@
 import { Button, RadioGroup } from "@chakra-ui/react";
-import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import PlanCard from "../../components/Cards/Plan";
 import { UserContext } from "../../context/UserContext";
+import { stepThreeSchema } from "../../helpers/validations/createUser";
+import useYupValidation from "../../hooks/useYupValidation";
 import { Actions } from "../../store/actions/User.actions";
+import { StepThreeValidation } from "../../types/UserValidation";
 import { plans } from "../../utils/plans";
 
 const StepPlans = () => {
+  const navigate = useNavigate();
+
   const { state, dispatch } = useContext(UserContext);
   const { data: values } = state;
 
+  const [errors, setErrors] = useState<StepThreeValidation>();
+
   const handleChange = (value: string) => {
     dispatch({ type: Actions.HANDLE_CHANGE, payload: { name: "plan_id", value } });
+  };
+
+  const handleNextStep = async () => {
+    const stepThree = { plan_id: values.plan_id };
+    useYupValidation({ data: stepThree, schema: stepThreeSchema }).then((res: any) => {
+      const { errors } = res;
+      if (errors) {
+        setErrors(errors);
+        return;
+      }
+      dispatch({ type: Actions.HANDLE_NEXT_STEP, payload: { step: "3" } });
+      navigate("/new-user/user-info");
+    });
   };
 
   return (
@@ -23,17 +43,21 @@ const StepPlans = () => {
           onChange={handleChange}
         >
           {plans.map((plan) => (
-            <PlanCard plan={plan} value={plan.id} selected={values.plan_id === plan.id} />
+            <PlanCard
+              key={plan.id}
+              plan={plan}
+              value={plan.id}
+              selected={values.plan_id === plan.id}
+            />
           ))}
         </RadioGroup>
+        <span className="text-center text-sm text-red-400">{errors?.plan_id?.[0]}</span>
 
         <div className="grid grid-cols-2 gap-4">
           <Link to={"/new-user/address"}>
             <Button className="w-full">Back</Button>
           </Link>
-          <Link to={"/new-user/user-info"}>
-            <Button className="w-full">Next</Button>
-          </Link>
+          <Button onClick={handleNextStep}>Next</Button>
         </div>
       </form>
     </main>
